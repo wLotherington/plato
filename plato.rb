cd8c90a8786ff4b9308f4fea1f7d4734c3143a4f
Will Lotherington <dev@kaplop.com>
2018-12-06 22:01:52 -0600

    add project files

--- plato.rb
+++ plato.rb
@@ -3,12 +3,13 @@ require 'sinatra/reloader'
 require 'tilt/erubis'
 require 'open-uri'
 require 'httparty'
+require 'dotenv/load'
 
 FULL_OPACITY = 1.5
 
 configure do
   enable :sessions
-  set :session_secret, 'session_key' # hide
+  set :session_secret, ENV['SESSIONKEY']
 end
 
 before do
@@ -16,27 +17,23 @@ before do
 end
 
 def google(form)
-  #
-  #
-  
+  api_key = ENV['APIKEY']
+  cx_code = ENV['CXCODE']
+
   results = HTTParty.get("https://www.googleapis.com/customsearch/v1?q=#{form}&cx=#{cx_code}&num=10&key=#{api_key}&searchType=image")
   results['items'].map { |item| item['link'] }
 end
 
 def valid_input?(form_name)
-  return false unless form_name.match(/\A[a-z ]*[a-z0-9]+[a-z ]*\z/i)
+  return false unless form_name =~ /\A[a-z ]*[a-z0-9]+[a-z ]*\z/i
   return false if session[:form_name] == cleaned(form_name)
   true
 end
 
 def add_to_history(form_name)
-  unless session[:history].include? form_name
-    session[:history] << form_name
-  end
+  session[:history] << form_name unless session[:history].include? form_name
 
-  while session[:history].size > 20
-    session[:history].shift
-  end
+  session[:history].shift while session[:history].size > 20
 end
 
 def cleaned(form_name)
@@ -49,7 +46,6 @@ get '/' do
 end
 
 get '/about' do
-  form_name = 'sailboat'
   erb :about
 end
 
